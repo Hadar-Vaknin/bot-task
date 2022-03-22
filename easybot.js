0fe91f4a16d1afcbe4709ea3ab59570e987a1420
Gal Shiff <galshiff@gmail.com>
2022-03-22 18:43:53 +0200

    feat :added add role

--- easybot.js
+++ easybot.js
@@ -1,53 +1,129 @@
-const { driver , api, settings } = require('@rocket.chat/sdk');
+const { driver, api, settings } = require('@rocket.chat/sdk');
+const { async } = require('q');
 const HOST = 'http://localhost:3000';
 const USER = 'CMD_BOT';
 const PASS = '1234';
-const BOTNAME = 'easybot';  
-settings.username=USER;
-settings.password=PASS;
-settings.host=HOST;
-var myuserid;
+const BOTNAME = 'easybot';
+settings.username = USER;
+settings.password = PASS;
+settings.host = HOST;
+let myuserid;
 
 const runbot = async () => {
+  const conn = await driver.connect();
+  myuserid = await driver.login({ username: USER, password: PASS });
 
-    const conn = await driver.connect();
+  await driver.subscribeToMessages();
+  console.log('subscribed');
 
-    myuserid = await driver.login({username: USER, password: PASS});
-    createPublicGroup();
+  driver.reactToMessages( processMessages );
+  console.log('connected and waiting for messages');
 }
 
 
-const processMessages = async(err, message, messageOptions) => {
+const processMessages = async (err, message, messageOptions) => {
   if (!err) {
     if (message.u._id === myuserid) return;
-
     const roomname = await driver.getRoomName(message.rid);
     if (message.msg.toLowerCase().startsWith(BOTNAME)) {
-      const response = message.u.username + 
-            ', how can ' + BOTNAME + ' help you with ' +
-            message.msg.substr(BOTNAME.length + 1);
-      //const sentmsg = await driver.sendDirectToUser(response , message.u.username)   
-        }        
+      parseMessage(message.msg)
+      const response = message.u.username +
+      ', how can ' + BOTNAME + ' help you with ' +
+      message.msg.substr(BOTNAME.length + 1);
+      const sentmsg = await driver.sendDirectToUser(response , message.u.username)   
     }
-}
-async function createPrivateGroup(){
-    
-    const msgObj={
-      "name":"groupTest",
-      "members":["HV"],
-      "readOnly": false
-    }
-    const res=await api.post("groups.create",JSON.stringify(msgObj));
-    
-}
-async function createPublicGroup(){
-    
-  const msgObj= {
-    "name":"publicChannelTest",
-    "members":[],
-    "readOnly": false
   }
-  const res=await api.post("channels.create",JSON.stringify(msgObj));
+}
+async function parseMessage(message){
+  const messageParts=message.split(';');
+  console.log(messageParts);
+  switch(messageParts[1]) {
+    case "create_room":
+      const users=[];
+      if(messageParts[4]!=''){
+        users=messageParts[4].split(',')
+      }
+      if(messageParts[2].toLowerCase()==='public'){
+        await createPublicRoom(messageParts[3] , users);
+      }
+      else if(messageParts[2].toLowerCase()==='private'){
+        await createPrivateRoom(messageParts[3] , users);
+      }
+      break;
+
+    case "set_user_active_status":
+      await setUserActiveStatus(messageParts[2], messageParts[3]);
+      break;
+
+    case "add_role_to_user":
+        await addRoleToUser(messageParts[2], messageParts[3]);
+        break;
+
+    case "remove_role_from_user":
+        await removeRoleFromUser(messageParts[2], messageParts[3]);
+        break;
+
+    default:
+      // code block
+  }
+}
+async function removeRoleFromUser(username , roleName)
+{
+  const payLoad = {
+    "username": username,
+    "roleName": roleName
+  }
+  const res = await api.post("roles.removeUserFromRole", payLoad);
+
   
+}
+async function addRoleToUser(username , roleName)
+{
+  const payLoad = {
+    "username": username,
+    "roleName": roleName
+  }
+  const res = await api.post("roles.addUserToRole", payLoad);
+
+  console.log(res)
+}
+async function setUserActiveStatus(username , activeStatus)
+{
+  const userId=await getUserId(username);
+  console.log(userId)
+  const payLoad = {
+    "userId": userId,
+    "activeStatus":Boolean(activeStatus)
+  }
+  const res = await api.post("users.setActiveStatus", payLoad);
+
+  console.log(res)
+}
+
+async function getUserId(username)
+{
+  const payLoad = {
+    "username": username,
+  }
+  const res = await api.get("users.info", payLoad);
+  if(res.error){
+    return res.error;
+  }
+  return res.user._id;
+}
+async function createPrivateRoom(roomName , members) {
+  const payLoad = {
+    "name": roomName,
+    "members": ["1"]
+  }
+  const res = await api.post("groups.create", payLoad);
+
+}
+async function createPublicRoom(roomName , members) {
+  const payLoad = {
+    "name": roomName,
+    "members": []
+  }
+  const res = await api.post("channels.create", payLoad);
 }
 runbot();
